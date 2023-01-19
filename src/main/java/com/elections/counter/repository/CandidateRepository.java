1b7070dac1dc069cce236330b3c9aa35ca8fdf0a
Cheo <barcecheo2319@gmail.com>
2023-01-19 12:20:00 -0500

    Configuring some settings

--- src/main/java/com/elections/counter/repository/CandidateRepository.java
+++ src/main/java/com/elections/counter/repository/CandidateRepository.java
@@ -2,7 +2,9 @@ package com.elections.counter.repository;
 
 import com.elections.counter.document.Candidate;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CandidateRepository extends MongoRepository<Candidate, String> {
 
 }
