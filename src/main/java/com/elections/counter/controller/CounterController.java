98865d4ce07012e0deacf81a59250b0468d4e009
Cheo <barcecheo2319@gmail.com>
2023-01-20 10:42:50 -0500

    Logging

--- src/main/java/com/elections/counter/controller/CounterController.java
+++ src/main/java/com/elections/counter/controller/CounterController.java
@@ -5,12 +5,13 @@ import com.elections.counter.dto.response.CandidateDto;
 import com.elections.counter.dto.response.CandidateResponse;
 import com.elections.counter.service.CounterService;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/counter_api/v1")
+@Slf4j
 public class CounterController {
 
   @Autowired
@@ -29,17 +31,23 @@ public class CounterController {
   @ResponseStatus(value = HttpStatus.CREATED)
   @ResponseBody
   public CandidateResponse createCandidate(@RequestBody CandidateRequest candidateRequest) {
+    log.info(String.format("POST createCandidate [request: %s]", candidateRequest));
+
     return counterService.createCandidate(candidateRequest);
   }
 
-  @PutMapping(path = "/candidate/{id}/votes/add")
+  @PatchMapping(path = "/candidate/{id}/votes/add")
   @ResponseStatus(value = HttpStatus.ACCEPTED)
   public long addVotes(@PathVariable String id, @RequestParam long votes) {
+    log.info(String.format("PATCH addVotes [id: %s, votes: %d]", id, votes));
+
     return counterService.addVotes(id, votes);
   }
 
   @GetMapping("/candidate")
   public List<CandidateDto> getCandidates() {
+    log.info("GET getCandidates");
+
     return counterService.getAllCandidates();
   }
 
